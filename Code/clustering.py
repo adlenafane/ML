dc43884de9076e58bd68b38fbafc226eda44e263
iNicoS <nicocenter@outlook.com>
2013-03-07 20:54:02 +0100

    Added a function to compute the score of a clustering

--- Code/clustering.py
+++ Code/clustering.py
@@ -30,3 +30,22 @@ def songIdsClustersList(clustersResult,rawData):
 			clusterList.append(rawData[indice][0][0])
 		result.append(clusterList)
 	return result
+
+def getClusteringScore(clusterList,similDic):
+	score=0
+	for cluster in clusterList:
+		for trackId in cluster:
+			similarSongs=similDic[trackId]
+			for similTrack in similarSongs:
+				if similTrack!=None:
+					if similTrack[0] in cluster:
+						score = score + similTrack[1]*1
+
+	return score
+#------------------------------------------------------------------------------ 
+#TESTS
+"""
+with open("/Users/nicolas/Documents/2012-2013/APA/Projet/ML2/similarityDic.txt", 'rb') as f:
+	X = cPickle.load(f)
+print getClusteringScore([['TRAAMPA128F92E7D0D','TRAYYAU128F92D58D0','TRBEBNV128F423783D']],X)
+"""
